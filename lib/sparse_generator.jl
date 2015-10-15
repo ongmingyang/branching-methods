83c1d13826a4ac1f3e691ae3a14c09f3bcaa11a7
Ming Yang <mingy@mit.edu>
2015-10-15 17:22:27 -0400

    Sparse graph generator

--- lib/sparse_generator.jl
+++ lib/sparse_generator.jl
@@ -2,21 +2,32 @@
 # A follows a normal distribution with mean 0 and std 1
 #
 function not_so_sparse(n)
-  A = zeros(n,n)
-  A = randn!(A)
+  A = randn(n,n)
   return A + A'
 end
 
 # Returns a sparse symmetric matrix
+# Representing a graph with maximum degree k
+#
+# @param n required to be even
 #
 function sparse_generate(n)
+  # n must be even
+  @assert ( n%2 == 0 )
+
   # k is arbitrary
   k = n
+
+  # Initialize M and A
   M = spzeros(n,n)
-  A = speye(n)
+  alt = int([i%2 for i=1:n-1])
+  zs = int(zeros(n))
+  A = Tridiagonal(alt, zs, alt)
+  A = sparse(full(A)) # Tridiagonal has no sparse cast
 
   for i=1:k
-    M += A[randcycle(n), randcycle(n)]
+    perm = randcycle(n)
+    M += A[perm, perm]
   end
   return M
 end
